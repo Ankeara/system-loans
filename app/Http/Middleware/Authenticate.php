0d53b6a30a433256b922d70968fd28c59aff51d8
DESKTOP-JQ1EBN4\Asus <ankeara017@gmail.com>
2025-04-23 18:55:36 +0700

    yes

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,6 +12,6 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : route('application.pages.auth.login');
+        return $request->expectsJson() ? null : route('application.pages.dashboard');
     }
 }
